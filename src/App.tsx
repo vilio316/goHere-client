006af176dc21a466626211175661c4516ecb7f7c
vilio316 <atembarry12@gmail.com>
2025-10-03 00:48:05 +0100

    AI Search Works, Bedtime

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,22 @@ import { SearchResult } from "./components/SearchResults"
 import type { resultType } from "./interfacesAndTypes"
 import GeocodedInfo from "./components/GeocodedInfo"
 import LoaderComp from "./components/LoaderComp"
+import FromAI from "./components/FromAI"
+import { FaHandSparkles } from "react-icons/fa6"
 
 function App() {
+  const [intermediateState, updateIntermediateState] = useState('')
   const [search_query, updateSearchQuery] = useState("")
   const [search_results, updateSearchResults] = useState<resultType[]>([])
   const [loaded, setLoaded] = useState(false)
-  const [AIResults, updateAIResults] = useState<string[]>(["Lorem"])
+  const [AIResults, updateAIResults] = useState<string[]>([])
   const [isUsingAI, setAIUseState ] = useState(true)
 
   useEffect(()=>{
     async function searchFromServer(query: string) {
       if(isUsingAI && query.length > 3){
         try{
-      const {data} = await axios.get(`http://localhost:8090/ai/${query}`)
+      const {data} : {data: any[]} = await axios.get(`http://localhost:8090/ai/${query}`)
       if(data.length > 0){
       console.log(data, typeof data, data[0])
       updateAIResults(data)
@@ -45,30 +48,33 @@ function App() {
     <>
     <div className="grid justify-items-center p-2 md:p-1">
       <GeocodedInfo />
-        <input type="text" name="search" id="location_query" required placeholder="Where do you wanna go?" className="rounded-4xl p-1 md:w-[90%] w-full block border-2 indent-4 h-[5rem] border-black outline-none peer invalid:border-2 invalid:border-red-500 autofocus" autoComplete="true" minLength={3}  onChange={(e) => {
+      <div className="grid gap-x-2 w-[90vw] grid-cols-12 items-center">
+        <button className="grid col-span-1" onClick={()=> setAIUseState(!isUsingAI)}>
+        <FaHandSparkles fill="pink" size={24}/>
+        </button>
+        <input type="text" name="search" id="location_query" required placeholder="Where do you wanna go?" className="rounded-4xl p-1 md:w-full col-span-9 w-full border-2 indent-4 h-[5rem] border-black outline-none peer invalid:border-2 invalid:border-red-500 autofocus" autoComplete="true" minLength={3}  onChange={(e) => {
          if(!isUsingAI){
          if(e.target.value.length %2 ==0){
             updateSearchQuery(e.target.value)
           }
         }
         else{
-          if(e.target.value.length >5 && e.target.value.length%3 == 0){
-            updateSearchQuery(e.target.value)
-          }
+            updateIntermediateState(e.target.value)
         }
 
         }}/>
-        <button className="ai_submit">
+        <button className={`ai_submit grid col-span-2 ${isUsingAI ? 'opacity-100': "opacity-0"}`} onClick={()=> {
+          updateSearchQuery(intermediateState)
+        }}>
         Submit
         </button>
+        </div>
         <p className="peer-invalid:block hidden peer-invalid:text-red-400 text-sm">
           Please enter a longer search term
         </p>
         {isUsingAI ? 
         <div className="search_results w-[90%]">
-              {AIResults.length > 0 && search_query.length > 5 ? AIResults.map((result) => <p key={result}>
-                {result}
-              </p>): search_query.length > 5 ? <LoaderComp/>: null}
+              {AIResults.length > 0 && search_query.length > 5 ? AIResults.map((result) => <FromAI query={result} />): search_query.length > 5 ? <LoaderComp/>: null}
         </div> :
         <div className="search_results w-[90%] ">
           {search_query.length > 3 && loaded ? search_results?.slice(0,5).map((item)=> (
---
+++ src/components/FromAI.tsx
@@ -0,0 +1,23 @@
+import axios from "axios"
+import type { resultType } from "../interfacesAndTypes"
+import { useEffect, useState } from "react"
+import { SearchResult } from "./SearchResults"
+
+export default function FromAI(props: {query: string}){
+    const [contents, updateContents] = useState({} as resultType)
+
+    useEffect(()=> {
+        async function getSingleItem(){
+            const {data} = await axios.get(`http://localhost:8090/search/${props.query}`)
+            updateContents(data[0])
+        }
+        getSingleItem()
+    }, [])
+
+    return(<>
+    {
+        contents.location ? <SearchResult item={contents} /> : null
+    }
+    </>
+    )
+}
